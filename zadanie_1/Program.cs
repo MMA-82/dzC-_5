d0786e6e552991315c84f03083fc0e3e2c5fddcd
Marat <brutto061@gmail.com>
2022-10-20 21:57:50 +0700

    Выполнили первое задание

--- zadanie_1/Program.cs
+++ zadanie_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Две прямые заданы уравнениями вида y = k*x + b");
+double[,] matrix = new double[2, 2];
+for(int i=0; i<matrix.GetLength(0); i++)
+{
+    for(int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+    }
+}
+Console.WriteLine("k1 = " + matrix[0, 0]);
+Console.WriteLine("k2 = " + matrix[0, 1]);
+Console.WriteLine("b1 = " + matrix[1, 0]);
+Console.WriteLine("b2 = " + matrix[1, 1]);
+Console.WriteLine();
+
+if(matrix[0, 0] == matrix[0, 1]) Console.WriteLine("Данные прямые не пересекаются!");
+else
+{
+    double x = (matrix[1, 1] - matrix[1, 0] / matrix[0, 0] - matrix[0, 1]);
+    double y = x * matrix[0, 0] + matrix[1, 0];
+    Console.WriteLine($"Координаты точки пересечения прямой: ({x}, {y})");
+} 
