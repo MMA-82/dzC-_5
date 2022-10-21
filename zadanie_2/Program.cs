7b398e1fb64a8a2e4594cb3f6e2cfcd1f752b261
Marat <brutto061@gmail.com>
2022-10-21 19:20:52 +0700

    Выполнили Задание №2

---
+++ zadanie_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Дана фигура с координатами вершин: ");
+int[] xarray = new int[4];
+int[] yarray = new int[4];
+for(int i = 0; i < xarray.Length; i++)
+{
+   xarray[i] = new Random().Next(-10, 10);
+   yarray[i] = new Random().Next(-10, 10);
+   Console.Write($"{(xarray[i], yarray[i])} ");     
+}
+Console.WriteLine();
+
+Console.Write("Введите коэффициент масштабирования k = ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+
+Console.Write("Масштабированная фигура имеет координаты вершин: ");
+for(int i = 0; i < xarray.Length; i++)
+{
+    xarray[i] = k * xarray[i];
+    yarray[i] = k * yarray[i];
+    Console.Write($"{(xarray[i], yarray[i])} "); 
+}
